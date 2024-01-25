b1efab3cb30d7b8d66278d5e12b5815288bf82f5
Alex-idk <alex@alexidk.dev>
2024-01-25 15:22:57 -0500

    main

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,141 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.CAN;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import me.nabdev.oxconfig.ConfigurableParameter;
+import me.nabdev.oxconfig.OxConfig;
+import me.nabdev.oxconfig.OxConfig.EditMode;
+import me.nabdev.oxconfig.OxConfig.EnsureMode;
+import me.nabdev.oxconfig.OxConfig.LoggingMode;
+
+/**
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
+ * project.
+ */
+public class Robot extends TimedRobot {
+  private static final String kDefaultAuto = "Default";
+  private static final String kCustomAuto = "My Auto";
+  private String m_autoSelected;
+  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+
+  private CANSparkMax shooterTop = new CANSparkMax(11, MotorType.kBrushless);
+  private CANSparkMax shooterBottom = new CANSparkMax(22, MotorType.kBrushless);
+  private CANSparkMax kicker = new CANSparkMax(33, MotorType.kBrushed);
+  private XboxController m_controller = new XboxController(0);
+
+  private ConfigurableParameter<Double> kickerSpeed = new ConfigurableParameter<Double>(0.35, "Kicker speed %");
+  private ConfigurableParameter<Double> TopShooterSpeed = new ConfigurableParameter<Double>(0.35, "Top shooter speed %");
+  private ConfigurableParameter<Double> BottomShooterSpeed = new ConfigurableParameter<Double>(0.35, "Bottom shooter speed %");
+
+
+  /**
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
+   */
+  @Override
+  public void robotInit() {
+    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
+    m_chooser.addOption("My Auto", kCustomAuto);
+    SmartDashboard.putData("Auto choices", m_chooser);
+    OxConfig.initialize();
+  }
+
+  /**
+   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
+   * that you want ran during disabled, autonomous, teleoperated and test.
+   *
+   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+   * SmartDashboard integrated updating.
+   */
+  @Override
+  public void robotPeriodic() {}
+
+  /**
+   * This autonomous (along with the chooser code above) shows how to select between different
+   * autonomous modes using the dashboard. The sendable chooser code works with the Java
+   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the chooser code and
+   * uncomment the getString line to get the auto name from the text box below the Gyro
+   *
+   * <p>You can add additional auto modes by adding additional comparisons to the switch structure
+   * below with additional strings. If using the SendableChooser make sure to add them to the
+   * chooser code above as well.
+   */
+  @Override
+  public void autonomousInit() {
+    m_autoSelected = m_chooser.getSelected();
+    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
+    System.out.println("Auto selected: " + m_autoSelected);
+  }
+
+  /** This function is called periodically during autonomous. */
+  @Override
+  public void autonomousPeriodic() {
+    switch (m_autoSelected) {
+      case kCustomAuto:
+        // Put custom auto code here
+        break;
+      case kDefaultAuto:
+      default:
+        // Put default auto code here
+        break;
+    }
+  }
+
+  /** This function is called once when teleop is enabled. */
+  @Override
+  public void teleopInit() {}
+
+  /** This function is called periodically during operator control. */
+  @Override
+  public void teleopPeriodic() {
+    if(m_controller.getLeftTriggerAxis() > 0.5) {
+      kicker.set(kickerSpeed.get());
+    } else {
+      kicker.set(0);
+    }
+    
+    if(m_controller.getRightTriggerAxis() > 0.5) {
+      shooterTop.set(TopShooterSpeed.get());
+      shooterBottom.set(BottomShooterSpeed.get());
+    } else {
+      shooterTop.set(0);
+      shooterBottom.set(0);
+    }
+  }
+
+  /** This function is called once when the robot is disabled. */
+  @Override
+  public void disabledInit() {}
+
+  /** This function is called periodically when disabled. */
+  @Override
+  public void disabledPeriodic() {}
+
+  /** This function is called once when test mode is enabled. */
+  @Override
+  public void testInit() {}
+
+  /** This function is called periodically during test mode. */
+  @Override
+  public void testPeriodic() {}
+
+  /** This function is called once when the robot is first started up. */
+  @Override
+  public void simulationInit() {}
+
+  /** This function is called periodically whilst in simulation. */
+  @Override
+  public void simulationPeriodic() {}
+}
